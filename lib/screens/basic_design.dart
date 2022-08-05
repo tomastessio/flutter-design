886259c05bd541009141874b734019f7591f82e6
Tomas Tessio <tomastessio@gmail.com>
2022-08-05 16:44:56 -0300

    Diseno basico finalizado

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -7,7 +7,24 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [Image(image: AssetImage('assets/river.jpg')), Title()],
+        children: [
+          // imagen
+          const Image(image: AssetImage('assets/river.jpg')),
+
+          // titulo
+          const Title(),
+
+          //boton
+          const ButtonSection(),
+
+          //descripcion
+          Container(
+            padding: const EdgeInsets.only(top: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Text(
+                'Ex in elit elit consequat. Qui amet elit sint nostrud cillum. Est irure velit eiusmod velit officia id ea sit. Anim ex aliquip ad Lorem consectetur excepteur sit ipsum aliquip quis. In amet cupidatat sit aliqua labore quis duis culpa. Consequat sit reprehenderit sunt proident incididunt culpa mollit exercitation adipisicing cupidatat magna et. Et laborum deserunt incididunt aliquip consectetur velit officia ullamco consequat aliqua commodo duis mollit.'),
+          )
+        ],
       ),
     );
   }
@@ -21,7 +38,7 @@ class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
       child: Row(
         children: [
           Column(
@@ -29,11 +46,11 @@ class Title extends StatelessWidget {
             children: const [
               Text(
                 'Estadio Antonio Vespucio Liberti',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
               Text(
                 'Juntos somos mas grandes',
-                style: TextStyle(color: Colors.black54),
+                style: TextStyle(color: Colors.black54, fontSize: 16),
               )
             ],
           ),
@@ -43,5 +60,60 @@ class Title extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            CustomButton(
+              icon: Icons.call,
+              texto: 'Reservar',
+            ),
+            CustomButton(
+              icon: Icons.location_on,
+              texto: 'Como llegar',
+            ),
+            CustomButton(
+              icon: Icons.share,
+              texto: 'Compartir',
+            )
+          ]),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String texto;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.texto,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Icon(
+        icon,
+        color: Colors.blue,
+        size: 30,
+      ),
+      Text(
+        texto,
+        style: const TextStyle(color: Colors.blue),
+      )
+    ]);
   }
 }
